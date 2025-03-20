ffdbfe71ed2c4526fdc061ab69e148e25866ab56
rax1mjon <tulaganovraximjon65@gmail.com>
2025-03-20 13:54:17 +0500

    first commit

---
+++ Js/main.js
@@ -0,0 +1,59 @@
+// const loading = document.getElementById("loading");
+
+// const loadingDuration = 5000;
+
+// setTimeout(() => {
+//   loading.classList.add("loading-none");
+// }, loadingDuration);
+
+window.addEventListener("scroll", function () {
+  shrink();
+});
+
+let navbar = document.getElementById("header");
+
+function shrink() {
+  if (scrollY > 100) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+    navbar.classList.remove("navbar-shrink");
+  }
+}
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   window.addEventListener("scroll", function () {
+//     toggleBacktop();
+//   });
+
+//   let backtop = document.getElementById("backtop");
+
+//   function toggleBacktop() {
+//     if (
+//       document.body.scrollTop > 150 ||
+//       document.documentElement.scrollTop > 150
+//     ) {
+//       backtop.classList.add("backtop-show");
+//     } else {
+//       backtop.classList.remove("backtop-show");
+//     }
+//   }
+// });
+
+// $(".owl-carousel").owlCarousel({
+//   loop: true,
+//   margin: 10,
+//   nav: true,
+//   responsive: {
+//     0: {
+//       items: 1,
+//     },
+//     600: {
+//       items: 1,
+//     },
+//     1000: {
+//       items: 1,
+//     },
+//   },
+// });
+
+// AOS.init();
